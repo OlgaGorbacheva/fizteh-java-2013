16be714b5f5444d1751c6e0c644bdf07058f6aa9
Olga <olga.gorbacheva@phystech.edu>
2013-12-26 09:27:48 +0400

    test8

--- src/ru/fizteh/fivt/students/olgagorbacheva/storable/StorableTableProviderFactory.java
+++ src/ru/fizteh/fivt/students/olgagorbacheva/storable/StorableTableProviderFactory.java
@@ -5,19 +5,19 @@ import java.io.IOException;
 
 import ru.fizteh.fivt.storage.structured.TableProviderFactory;
 
-public class StorableTableProviderFactory implements TableProviderFactory{
-      
+public class StorableTableProviderFactory implements TableProviderFactory {
+
       @Override
       public StorableTableProvider create(String dir) throws IllegalArgumentException, IOException {
             if (dir == null || dir.trim().equals("") || dir.isEmpty()) {
                   throw new IllegalArgumentException("Недопустимое имя хранилища базы данных");
             }
             File directory = new File(dir);
-//            if (!directory.exists()) {
-//                  throw new IOException("Директории с данным именем не существует");
-//            }
             if (!directory.isDirectory()) {
-                  throw new IOException("Файл с данным именем не является директорией");
+                  if (!directory.exists()) {
+                        throw new IOException("Директории с данным именем не существует");
+                  }
+                  throw new IllegalArgumentException("Файл с данным именем не является директорией");
             }
             if (!directory.canRead() || !directory.canWrite()) {
                   throw new RuntimeException("Директория недоступна");
